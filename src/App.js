83445b32982940b81f67ab3c57df8670860457f9
ChristianThuranira <christianthuranira@gmail.com>
2024-11-13 08:44:33 +0300

    changes.env

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>
         Mambo sawa
-        
+
       </h1>
              </div>
   );
