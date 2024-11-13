7a58d85c76b9332d27b9d26734a722b436f849e7
ChristianThuranira <christianthuranira@gmail.com>
2024-11-13 16:36:30 +0300

    deployments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+
 import './App.css';
+import Home from './pages/Home';
+import DetailExercise from './pages/DetailExercise';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width= "400px" sx={{width: {xl: '1488px'}}} m = "auto">
+        <Navbar/>
+        <Routes>
+            <Route path = "/" element = { <Home/>} />
+            <Route path = "/exercise/:id" element = { <DetailExercise/>} />
+        </Routes>
+
+        <Footer/>
+    </Box>
+    
+
+    
   )
 }
 
